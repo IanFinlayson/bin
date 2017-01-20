6a88ad58adf5a461f06b986ef51426977309fef4
Ian Finlayson <finlaysoni@gmail.com>
2017-01-20 10:49:56 -0500

    Added some new things

---
+++ email-copier.c
@@ -0,0 +1,19 @@
+/* I use this program as my default email client, because when I try to click
+ * email links, often I just want to be able to COPY THE DAMN EMAIL address
+ * which is hard to do in thunderbird or mutt.  This just writes them to a text
+ * file in my home directory */
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    FILE* f = fopen("/home/finlayson/emails.txt", "w");
+    if (!f) {return 23;}
+
+    for (int i = 1; i < argc; i++) {
+        fprintf(f, "%s\n", argv[i]);
+    }
+    fclose(f);
+
+    return 0;
+}
+
